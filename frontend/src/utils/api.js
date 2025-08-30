804e1e67390b0391bf207ff399b19b381351dbae
evardaan0 <evardaan0@gmail.com>
2025-08-30 16:45:51 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
 
 import axios from 'axios';
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api' });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'https://vardaan-d6l4.onrender.com/api' });
 export default api;
