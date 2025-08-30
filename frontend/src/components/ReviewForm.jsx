794a1eb54a6a5b1b6284fc2d4416b1325e99e467
evardaan0 <evardaan0@gmail.com>
2025-08-30 16:35:18 +0530

    Update ReviewForm.jsx

--- frontend/src/components/ReviewForm.jsx
+++ frontend/src/components/ReviewForm.jsx
@@ -17,7 +17,7 @@ export default function ReviewForm({ onReviewAdded }) {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/newReview",
+        "https://vardaan-d6l4.onrender.com/api/newReview",
         formData
       );
       onReviewAdded(res.data);
