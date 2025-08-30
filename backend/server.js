09a8b0ae9228cb347b021984c24547adbb5550f4
evardaan0 <evardaan0@gmail.com>
2025-08-30 16:57:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const url = process.env.MONGO_URL;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["https://vardaanenterprise.in"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
